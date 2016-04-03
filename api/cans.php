415cff594a3e48074d87d442f9b8fe59750cb59f
Matthew Cipperly <mc@tinyatom.net>
2016-04-03 17:22:05 -0400

    serialize data as json

--- api/cans.php
+++ api/cans.php
@@ -1,9 +1,16 @@
 <?php
 require_once('../db/db.php');
 
+header('Content-type: application/json');
+
 switch ($_SERVER['REQUEST_METHOD']) {
   case 'GET':
-    print_r(get_cans());
+    $cans = get_cans();
+    $resp = array();
+    foreach($cans as $can) {
+      $resp[] = array("id" => $can['can_id'], "type" => $can['type_id'], "lat" => $can['latitude'], "lng" => $can['longitude']);
+    }
+    print(json_encode(array("cans" => $resp), JSON_NUMERIC_CHECK));
     break;
 }
 
