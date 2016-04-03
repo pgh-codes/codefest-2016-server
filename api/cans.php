ca67fa706fa044c702a9b49da0f0e1292d856d5a
Matthew Cipperly <mc@tinyatom.net>
2016-04-03 19:59:02 -0400

    cans and users and login

--- api/cans.php
+++ api/cans.php
@@ -2,6 +2,7 @@
 require_once('../db/db.php');
 
 header('Content-type: application/json');
+header('Access-Control-Allow-Origin: *');
 
 switch ($_SERVER['REQUEST_METHOD']) {
   case 'GET':
@@ -11,6 +12,24 @@ switch ($_SERVER['REQUEST_METHOD']) {
       $resp[] = array("id" => $can['can_id'], "type" => $can['type_id'], "lat" => $can['latitude'], "lng" => $can['longitude']);
     }
     print(json_encode(array("cans" => $resp), JSON_NUMERIC_CHECK));
+    break;
+  case 'POST':
+    $req = file_get_contents('php://input');
+
+    if($req == "" || $req == null) {
+      print(json_encode(array("success" => false, "error" => "No data received", "canId" => "")));
+      break;
+    }
+
+    $can_info = json_decode($req, true);
+
+    if($_REQUEST['canId'] != "") {
+      $can_id = $_REQUEST['canId'];
+      update_can($can_id, $can_info['type'], $can_info['lat'], $can_info['lng']);
+    } else {
+      $can_id = add_can($can_info['type'], $can_info['lat'], $can_info['lng']);
+    }
+    print(json_encode(array("success" => true, "error" => "", "canId" => $can_id)));
     break;
 }
 
