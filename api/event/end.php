02687360543da127d231c3a30398decc8cca839d
Matthew Cipperly <mc@tinyatom.net>
2016-04-05 22:17:16 -0400

    end event endpoint

---
+++ api/event/end.php
@@ -0,0 +1,33 @@
+<?php
+require_once('../../db/db.php');
+
+header('Content-type: application/json');
+header('Access-Control-Allow-Origin: *');
+
+switch ($_SERVER['REQUEST_METHOD']) {
+  case 'GET':
+    if(isset($_GET['canId']) && is_numeric($_GET['canId'])) {
+      session_start();
+      if(isset($_SESSION['info'])) {
+        $cur_user = get_user($_SESSION['info']);
+        if(($cur_user['type_id'] % 2) == 0) {
+          end_event($_GET['canId']);
+          print(json_encode(array("success" => "true", "error" => "")));
+          break;
+        } else {
+          print(json_encode(array("success" => "false", "error" => "Improper user type")));
+          break;
+        }
+      } else {
+        print(json_encode(array("success" => "false", "error" => "Not logged in")));
+        break;
+      }
+    } else {
+      print(json_encode(array("success" => "false", "error" => "Invalid request parameters")));
+      break;
+    }
+  default:
+    print(json_encode(array("success" => false, "error" => "Unsupported request method")));
+    break;
+
+  }
