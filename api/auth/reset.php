46a642fe5cf1e214bb362146ef267b55afdd60c2
Matthew Cipperly <mc@tinyatom.net>
2016-04-05 21:40:43 -0400

    reset password, new event, new user requests

--- api/auth/reset.php
+++ api/auth/reset.php
@@ -1,23 +1,10 @@
 <?php
-require_once('../db/db.php');
+require_once('../../db/db.php');
+
 header('Content-type: application/json');
 header('Access-Control-Allow-Origin: *');
 
-
 switch($_SERVER['REQUEST_METHOD']) {
-  case GET:
-    if(is_numeric($_GET['userId'])) {
-      $users = get_user($_GET['userId']);
-    } else {
-      $users = get_users();
-    }
-    $resp = array();
-    foreach($users as $user) {
-      $resp[] = array("username" => $user['firstname'] . " " . $user['lastname'], "userid" => $user['user_id'], "type" => $user['type_id']);
-    }
-
-    print(json_encode(array("users" => $resp), JSON_NUMERIC_CHECK));
-    break;
   case POST:
     $req_json = file_get_contents('php://input');
 
@@ -27,23 +14,20 @@ switch($_SERVER['REQUEST_METHOD']) {
     } else {
       $req = json_decode($req_json, true);
     }
-    if(!isset($req['firstname'],$req['lastname'],$req['type'])) {
-      print(json_encode(array("success" => false, "error" => "Incomplete data received")));
-      break;
-    }
+
     session_start();
     if(isset($_SESSION['info'])) {
       $cur_user = get_user($_SESSION['info']);
       if($cur_user['type_id'] >= 2) {
-        $new_password = add_user($req['firstname'], $req['lastname'], $req['type']);
+        $new_password = reset_password($req['userid']);
         print(json_encode(array("success" => "true", "error" => "", "newPassword" => $new_password)));
         break;
       }
     }
-
+    print(json_encode(array("success" => "false", "error" => "Invalid Credentials")));
+    break;
   default:
     print(json_encode(array("success" => false, "error" => "Unsupported request method")));
     break;
-
 }
  ?>
---
+++ api/event/new.php
@@ -0,0 +1,34 @@
+<?php
+require_once('../../db/db.php');
+
+header('Content-type: application/json');
+header('Access-Control-Allow-Origin: *');
+
+switch($_SERVER['REQUEST_METHOD']) {
+  case POST:
+    $req_json = file_get_contents('php://input');
+
+    if($req_json == "" || $req_json == null) {
+      print(json_encode(array("success" => false, "error" => "No data received")));
+      break;
+    } else {
+      $req = json_decode($req_json, true);
+    }
+    if(!isset($req['canId'],$req['bags'])) {
+      print(json_encode(array("success" => false, "error" => "Incomplete data received")));
+      break;
+    }
+    session_start();
+    if(isset($_SESSION['info']) && $_SESSION['info'] > 0 ) {
+      $new_event = start_event($req['canId'], $req['bags'], $_SESSION['info'], $req['note'] = null);
+      print(json_encode(array("success" => "true", "error" => "", "eventId" => $new_event)));
+      break;
+    } else {
+      print(json_encode(array("success" => "false", "error" => "Invalid Credentials")));
+    }
+    break;
+  default:
+    print(json_encode(array("success" => false, "error" => "Unsupported request method")));
+    break;
+}
+ ?>
