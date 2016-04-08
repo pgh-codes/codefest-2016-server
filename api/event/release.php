3341ca1e0e481a738ea5ccb179cab5434ec8b2a3
Rob Collini <topia17@gmail.com>
2016-04-07 20:14:58 -0400

    added release event API

---
+++ api/event/release.php
@@ -0,0 +1,32 @@
+<?php
+require_once('../../db/db.php');
+
+header('Content-type: application/json');
+header('Access-Control-Allow-Origin: *');
+
+switch ($_SERVER['REQUEST_METHOD']) {
+  case 'GET':
+    if(isset($_GET['canId']) && is_numeric($_GET['canId'])) {
+      session_start();
+      if(isset($_SESSION['info'])) {
+        if(($_SESSION['info']['type_id'] % 2) == 0) {
+          release_events($_SESSION['info']['user_id']);
+          print(json_encode(array("success" => "true", "error" => "")));
+          break;
+        } else {
+          print(json_encode(array("success" => "false", "error" => "Improper user type")));
+          break;
+        }
+      } else {
+        print(json_encode(array("success" => "false", "error" => "Not logged in")));
+        break;
+      }
+    } else {
+      print(json_encode(array("success" => "false", "error" => "Invalid request parameters")));
+      break;
+    }
+  default:
+    print(json_encode(array("success" => false, "error" => "Unsupported request method")));
+    break;
+
+  }
